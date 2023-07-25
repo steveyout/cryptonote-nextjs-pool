108d923c67cd011a2ab226b9cb195b485791d588
youtsteve1@gmail.com <youtsteve1@gmail.com>
2023-07-25 13:08:55 +0300

    updated workers.js

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteUrl: process.env.NEXT_PUBLIC_API || 'https://uplexa.bigminers.site',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+  changefreq: 'daily',
+  priority: 0.7,
+};
